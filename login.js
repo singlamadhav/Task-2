5cb361800d66ba3121688810c634e0f81099b5e0
herman nagpal <hermannagpal7682@gmail.com>
2024-04-24 14:22:31 +0530

    login.js

---
+++ login.js
@@ -0,0 +1,7 @@
+$(window).on("hashchange", function () {
+	if (location.hash.slice(1) == "signup") {
+		$(".page").addClass("extend");
+		$("#login").removeClass("active");
+		$("#signup").addClass("active");
+	} else {
+
