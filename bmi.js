5b3cd6fa415e09f0be842f05badb0eb4fa568a2f
singlamadhav <madhavsingla271@gmail.com>
2024-04-24 13:54:07 +0530

    bmi.js update

--- bmi.js
+++ bmi.js
@@ -4,3 +4,8 @@ const bmi_value = document.querySelector('#bmi-value');
 const bmi_status = document.querySelector('#bmi-status');
 const calculate = document.querySelector('#calculate-bmi');
 var dietPlanLink = document.getElementById('dietPlanLink');
+calculate.addEventListener('click', () => {
+   if( height.value === "" || weight.value === "" ) {
+        alert('Please enter your height and weight');
+        return;
+   }else{
