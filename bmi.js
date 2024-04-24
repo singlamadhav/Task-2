caa2d6f715daab01981d8534c4028b9d925548d6
singlamadhav <madhavsingla271@gmail.com>
2024-04-24 13:52:47 +0530

    file bmi.js

---
+++ bmi.js
@@ -0,0 +1,4 @@
+const height = document.querySelector('#height');
+const weight = document.querySelector('#weight');
+const bmi_value = document.querySelector('#bmi-value');
+const bmi_status = document.querySelector('#bmi-status');
