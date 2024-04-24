096b414d954578970e7449cc75ce3fd1aca82aef
herman nagpal <hermannagpal7682@gmail.com>
2024-04-24 13:48:19 +0530

    new added

--- feedback.js
+++ feedback.js
@@ -3,5 +3,11 @@ const ratingsContainer = document.querySelector('.ratings-container')
 const sendBtn = document.querySelector('#send')
 const panel = document.querySelector('#panel')
 let selectedRating = 'Satisfied'
+ratingsContainer.addEventListener('click', (e) => {
+    if(e.target.parentNode.classList.contains('rating')) {
+        removeActive()
+        e.target.parentNode.classList.add('active')
+        selectedRating = e.target.nextElementSibling.innerHTML
+    }
 
 
