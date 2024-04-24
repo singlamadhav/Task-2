72e589272d734034587fba0317ee648a3e7e7d2e
herman nagpal <hermannagpal7682@gmail.com>
2024-04-24 14:23:09 +0530

    login.js update

--- login.js
+++ login.js
@@ -4,4 +4,56 @@ $(window).on("hashchange", function () {
 		$("#login").removeClass("active");
 		$("#signup").addClass("active");
 	} else {
+$(window).on("hashchange", function () {
+        if (location.hash.slice(1) == "signup") {
+                $(".page").addClass("extend");
+                $("#login").removeClass("active");
+                $("#signup").addClass("active");
+        } else {
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+login.js [unix] (14:21 24/04/2024)                                                                                                                                 7,0-1 All
+"login.js" [unix] 7L, 200B
 
