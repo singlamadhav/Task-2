754c9109bccb0675295c86c5322fab5bbc0c1406
herman nagpal <hermannagpal7682@gmail.com>
2024-04-24 14:26:55 +0530

    ogin.js update again

--- login.js
+++ login.js
@@ -39,6 +39,9 @@ function validateSignupForm() {
 		document.getElementById("errorMsg").innerHTML = "Please fill the required fields"
 		return false;
 	}
+	else if (password.length < 8) {
+		document.getElementById("errorMsg").innerHTML = "Your password must include atleast 8 characters"
+		return false;
 
 ~
 ~
