ba3c82475275b5048e784dbc75a139adb9a94870
nikhil4467 <nikhil0836.be23@chitkara.edu.in>
2024-04-24 14:42:37 +0530

    done

--- home.js
+++ home.js
@@ -1,1 +1,64 @@
-let menu = document.querySelector('#menu-btn')let menu = document.querySelector('#menu-btn')
+let menu = document.querySelector('#menu-btn')
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+home.js [unix] (14:40 24/04/2024)                                                                                                                                                                         1,92 All
+"home.js" [unix] 1L, 93B
+
